d0f8d8d9846e427d3436cfda5abc0ad6e3691d20
小贝 <whb@wuhuabeideMac-mini.local>
2020-11-09 10:50:25 +0800

    tmp

--- src/components/Home/index.vue
+++ src/components/Home/index.vue
@@ -1,10 +1,30 @@
 <template>
-  
+  <h1>111111</h1>
 </template>
 
 <script>
 export default {
-  name: '',
+  name: 'Home',
+  mounted() {
+    var ws = new WebSocket("ws://172.20.1.30:6969/dashboard")
+    console.log(111111)
+    ws.onopen = function() {
+      console.log(2222222222)
+      // ws.send('111111')
+    }
+
+    ws.onmessage = function() {
+      console.log(333333)
+    }
+
+    ws.onclose = function() {
+      console.log('close')
+    }
+
+    ws.onerror = function() {
+      console.log(4444444)
+    }
+  },
 }
 </script>
 
