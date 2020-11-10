770c535d67bb7c39aead86d03833986439e83eae
小贝 <whb@wuhuabeideMac-mini.local>
2020-11-10 15:17:40 +0800

    tmp

--- src/components/Home/index.vue
+++ src/components/Home/index.vue
@@ -1,8 +1,19 @@
 <template>
    <div id="components-layout-demo-basic">
     <a-layout>
-      <a-layout-header>Header</a-layout-header>
-      <a-layout-content>Content</a-layout-content>
+      <a-layout-header>publisher-dashboard</a-layout-header>
+      <a-layout-content>
+        <a-select placeholder="namespace" style="width: 200px" @change="handleChange">
+          <a-select-option v-for="namespace in namespaces" :key="namespace" :value="namespace">
+            {{namespace}}
+          </a-select-option>
+        </a-select>
+        <a-select placeholder="group" style="width: 200px;margin-left: 20px;" @change="onchange">
+          <a-select-option v-for="group in groups" :key="group" :value="group">
+            {{group}}
+          </a-select-option>
+        </a-select>
+      </a-layout-content>
     </a-layout>
   </div>
 </template>
@@ -12,33 +23,34 @@ export default {
   name: 'Home',
   data() {
     return {
-      // proto: ''
+      namespaces: '',
+      groups: '',
+      cur_namespace: '',
+      cur_group: '',
     }
   },
   mounted() {
 
-    this.initData()
+    this.initNameSpace()
   },
   methods: {
-    initData() {
+    initNameSpace() {
 
       var proto = this.$proto.github.com.nevercase.publisher.pkg.types
 
-      let arr = ['ListNamespace','ListGroupName','ListTask']
+      // let arr = ['ListNamespace','ListGroupName','ListTask']
 
-      arr.forEach(element => {
-        let data = {
-          body: "Dashboard",
-          serviceApi: element
-        }
+      let data = {
+          body: 'Dashboard',
+          serviceApi: 'ListNamespace'
+      }
 
-        this.initQuest(data, proto)
-      });
+      this.initQuest(data, proto)
 
     },
-    initQuest(msg, proto) {
+    initQuest(msg, proto, other_data = '') {
       let message = {
-        data: '', 
+        data: other_data, 
         type: msg
       }
 
@@ -57,13 +69,12 @@ export default {
 
       switch (message.type.serviceApi) {
         case 'ListNamespace':
-          let namespace = proto.ListNamespaceResponse.decode(message.data)
-
-          console.log(namespace)
+          let namespaces = proto.ListNamespaceResponse.decode(message.data)
+            _self.namespaces = namespaces.items
           break
         case 'ListGroupName':
           let group = proto.ListGroupNameResponse.decode(message.data)
-          console.log(group)
+          _self.groups = group.items
           break
         case 'ListTask':
           let task = proto.ListTaskResponse.decode(message.data)
@@ -73,42 +84,54 @@ export default {
           break
         
       }
-      
+    },
+    handleChange(value) {
+      this.cur_namespace = value
+      this.cur_group = ''
 
+      var proto = this.$proto.github.com.nevercase.publisher.pkg.types
+      let data = {
+          body: 'Dashboard',
+          serviceApi: 'ListGroupName'
+      }
+
+      let other_data = {
+        namespace: value
+      }
+        
+      let init = proto.ListGroupNameRequest.create(other_data)
+      this.initQuest(data, proto, proto.ListGroupNameRequest.encode(init).finish())
+    },
+    onchange(value) {
+      this.cur_group = value
+      console.log(value)
     }
-    
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  #components-layout-demo-basic {
+#components-layout-demo-basic {
   text-align: center;
+  width: 100%;
+  height: 100%;
 }
 #components-layout-demo-basic .ant-layout-header,
 #components-layout-demo-basic .ant-layout-footer {
-  background: #7dbcea;
+  background: #161b1f;
   color: #fff;
 }
-#components-layout-demo-basic .ant-layout-footer {
-  line-height: 1.5;
+
+#components-layout-demo-basic .ant-layout {
+  width: 100%;
+  height: 100%;
 }
-#components-layout-demo-basic .ant-layout-sider {
-  background: #3ba0e9;
-  color: #fff;
-  line-height: 120px;
+#components-layout-demo-basic .ant-select,.ant-select-enabled {
+  margin-top: 20px;
 }
-#components-layout-demo-basic .ant-layout-content {
-  background: rgba(16, 142, 233, 1);
-  color: #fff;
-  min-height: 120px;
-  line-height: 120px;
-}
-#components-layout-demo-basic > .ant-layout {
-  margin-bottom: 48px;
-}
-#components-layout-demo-basic > .ant-layout:last-child {
-  margin: 0;
+
+.ant-select-selection--single {
+  width: 180px;
 }
 </style>
